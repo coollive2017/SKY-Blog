14006d58b98a52b61c0374a4b2092337aa945393
coollive2017 <coollive2017@163.com>
2018-05-11 22:56:01 +0800

    uqdata blog index and add admin page

---
+++ app/controllers/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Article = mongoose.model('Article');
+
+module.exports = (app) => {
+  app.use('/admin', router);
+};
+
+router.get('/', (req, res, next) => {
+  Article.find((err, articles) => {
+    if (err) return next(err);
+    res.render('admin/index', {
+      title: 'Sky-Blog-Admin',
+      articles: articles,
+      pretty:true
+    });
+  });
+});
